4c2993ba42dcde2a6d93aeb066b0b2657d48aacb
Davis <zanxiaofeng@163.com>
2025-11-12 21:00:40 +0800

    update

--- zxf-springboot-trace-webmvc/src/main/java/zxf/trace/support/trace/TraceConfiguration.java
+++ zxf-springboot-trace-webmvc/src/main/java/zxf/trace/support/trace/TraceConfiguration.java
@@ -3,8 +3,10 @@ package zxf.trace.support.trace;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.http.client.BufferingClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 import zxf.trace.OutboundLoggingInterceptor;
 
@@ -13,9 +15,17 @@ public class TraceConfiguration {
     @Autowired
     private OutboundLoggingInterceptor outboundLoggingInterceptor;
 
-
     @Bean
+    @Primary
     public RestTemplate restTemplate() {
+        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+        RestTemplate restTemplate = new RestTemplate(requestFactory);
+        restTemplate.getInterceptors().add(outboundLoggingInterceptor);
+        return restTemplate;
+    }
+
+    @Bean("restTemplateWithBuffer")
+    public RestTemplate restTemplateWithBuffer() {
         HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
         RestTemplate restTemplate = new RestTemplate(new BufferingClientHttpRequestFactory(requestFactory));
         restTemplate.getInterceptors().add(outboundLoggingInterceptor);
